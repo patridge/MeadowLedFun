fc44d67838735d08df31d4e28e05b8c73c18f4b4
Adam Patridge <patridge@gmail.com>
2024-05-18 14:04:10 -0600

    New ProjectLab template

---
+++ DisplayController.cs
@@ -0,0 +1,206 @@
+﻿using Meadow;
+using Meadow.Foundation;
+using Meadow.Foundation.Graphics;
+using Meadow.Peripherals.Displays;
+using Meadow.Units;
+
+namespace LedFun;
+
+public class DisplayController
+{
+    private bool isUpdating = false;
+    private bool needsUpdate = false;
+
+    private readonly string hardwareRev;
+    private readonly MicroGraphics graphics;
+
+    public Temperature? Temperature
+    {
+        get => temperature;
+        set
+        {
+            temperature = value;
+            Update();
+        }
+    }
+    private Temperature? temperature;
+
+    public RelativeHumidity? RelativeHumidity
+    {
+        get => relativeHumidity;
+        set
+        {
+            relativeHumidity = value;
+            Update();
+        }
+    }
+    private RelativeHumidity? relativeHumidity;
+
+    public Pressure? Pressure
+    {
+        get => pressure;
+        set
+        {
+            pressure = value;
+            Update();
+        }
+    }
+    private Pressure? pressure;
+
+    public Illuminance? LightConditions
+    {
+        get => lightConditions;
+        set
+        {
+            lightConditions = value;
+            Update();
+        }
+    }
+    private Illuminance? lightConditions;
+
+    public Acceleration3D? AccelerationConditions
+    {
+        get => accelerationConditions;
+        set
+        {
+            accelerationConditions = value;
+            Update();
+        }
+    }
+    private Acceleration3D? accelerationConditions;
+
+    public AngularVelocity3D? GyroConditions
+    {
+        get => gyroConditions;
+        set
+        {
+            gyroConditions = value;
+            Update();
+        }
+    }
+    private AngularVelocity3D? gyroConditions;
+
+    public bool UpButtonState
+    {
+        get => upButtonState;
+        set
+        {
+            upButtonState = value;
+            Update();
+        }
+    }
+    private bool upButtonState = false;
+
+    public bool DownButtonState
+    {
+        get => downButtonState;
+        set
+        {
+            downButtonState = value;
+            Update();
+        }
+    }
+    private bool downButtonState = false;
+
+    public bool LeftButtonState
+    {
+        get => leftButtonState;
+        set
+        {
+            leftButtonState = value;
+            Update();
+        }
+    }
+    private bool leftButtonState = false;
+
+    public bool RightButtonState
+    {
+        get => rightButtonState;
+        set
+        {
+            rightButtonState = value;
+            Update();
+        }
+    }
+    private bool rightButtonState = false;
+
+    public DisplayController(IPixelDisplay display, string hardwareRevision)
+    {
+        hardwareRev = hardwareRevision;
+        graphics = new MicroGraphics(display)
+        {
+            CurrentFont = new Font12x20()
+        };
+
+        graphics.Clear(true);
+    }
+
+    public void Update()
+    {
+        if (isUpdating)
+        {   //queue up the next update
+            needsUpdate = true;
+            return;
+        }
+
+        isUpdating = true;
+
+        graphics.Clear();
+        Draw();
+        graphics.Show();
+
+        isUpdating = false;
+
+        if (needsUpdate)
+        {
+            needsUpdate = false;
+            Update();
+        }
+    }
+
+    private void DrawStatus(string label, string value, Color color, int yPosition)
+    {
+        graphics.DrawText(x: 2, y: yPosition, label, color: color);
+        graphics.DrawText(x: graphics.Width - 2, y: yPosition, value, alignmentH: HorizontalAlignment.Right, color: color);
+    }
+
+    private void Draw()
+    {
+        graphics.DrawText(x: 2, y: 0, $"Hello PROJ LAB {hardwareRev}!", WildernessLabsColors.AzureBlue);
+
+        if (Temperature is { } temp)
+        {
+            DrawStatus("Temperature:", $"{temp.Celsius:N1}°C", WildernessLabsColors.GalleryWhite, 35);
+        }
+
+        if (Pressure is { } pressure)
+        {
+            DrawStatus("Pressure:", $"{pressure.StandardAtmosphere:N1}atm", WildernessLabsColors.GalleryWhite, 55);
+        }
+
+        if (RelativeHumidity is { } humidity)
+        {
+            DrawStatus("Humidity:", $"{humidity.Percent:N1}%", WildernessLabsColors.GalleryWhite, 75);
+        }
+
+        if (LightConditions is { } light)
+        {
+            DrawStatus("Lux:", $"{light:N0}Lux", WildernessLabsColors.GalleryWhite, 95);
+        }
+
+        if (AccelerationConditions is { } acceleration)
+        {
+            DrawStatus("Accel:", $"{acceleration.X.Gravity:0.#},{acceleration.Y.Gravity:0.#},{acceleration.Z.Gravity:0.#}g", WildernessLabsColors.AzureBlue, 115);
+        }
+
+        if (GyroConditions is { } angular3D)
+        {
+            DrawStatus("Gyro:", $"{angular3D.X:0},{angular3D.Y:0},{angular3D.Z:0}rpm", WildernessLabsColors.AzureBlue, 135);
+        }
+
+        DrawStatus("Left:", $"{(LeftButtonState ? "pressed" : "released")}", WildernessLabsColors.ChileanFire, 200);
+        DrawStatus("Down:", $"{(DownButtonState ? "pressed" : "released")}", WildernessLabsColors.ChileanFire, 180);
+        DrawStatus("Up:", $"{(UpButtonState ? "pressed" : "released")}", WildernessLabsColors.ChileanFire, 160);
+        DrawStatus("Right:", $"{(RightButtonState ? "pressed" : "released")}", WildernessLabsColors.ChileanFire, 220);
+    }
+}
---
+++ MeadowApp.cs
@@ -0,0 +1,188 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Audio;
+using Meadow.Units;
+using System;
+using System.Threading.Tasks;
+
+namespace LedFun;
+
+// Change F7FeatherV2 to F7FeatherV1 if using Feather V1 Meadow boards
+// Change to F7CoreComputeV2 for Project Lab V3.x
+public class MeadowApp : App<F7CoreComputeV2>
+{
+    private IProjectLabHardware? projectLab;
+
+    private DisplayController? displayController;
+
+    private MicroAudio? audio;
+
+    public override Task Initialize()
+    {
+        Resolver.Log.LogLevel = Meadow.Logging.LogLevel.Trace;
+
+        Resolver.Log.Info("Initialize hardware...");
+
+        projectLab = ProjectLab.Create();
+
+        Resolver.Log.Info($"Running on ProjectLab Hardware {projectLab.RevisionString}");
+
+        if (projectLab.RgbLed is { } rgbLed)
+        {
+            rgbLed.SetColor(Color.Blue);
+        }
+
+        if (projectLab.Display is { } display)
+        {
+            Resolver.Log.Trace("Creating DisplayController");
+            displayController = new DisplayController(display, projectLab.RevisionString);
+            Resolver.Log.Trace("DisplayController up");
+        }
+
+        if (projectLab.Speaker is { } speaker)
+        {
+            speaker.SetVolume(0.5f);
+            audio = new MicroAudio(speaker);
+        }
+
+        if (projectLab.TemperatureSensor is { } temperatureSensor)
+        {
+            temperatureSensor.Updated += OnTemperatureSensorUpdated;
+        }
+        if (projectLab.BarometricPressureSensor is { } pressureSensor)
+        {
+            pressureSensor.Updated += OnPressureSensorUpdated;
+        }
+        if (projectLab.HumiditySensor is { } humiditySensor)
+        {
+            humiditySensor.Updated += OnHumiditySensorUpdated;
+        }
+        if (projectLab.LightSensor is { } lightSensor)
+        {
+            lightSensor.Updated += OnLightSensorUpdated;
+        }
+
+        if (projectLab.Accelerometer is { } accelerometer)
+        {
+            accelerometer.Updated += OnAccelerometerUpdated;
+        }
+        if (projectLab.Gyroscope is { } gyroscope)
+        {
+            gyroscope.Updated += OnGyroscopeUpdated;
+        }
+
+        if (projectLab.UpButton is { } upButton)
+        {
+            upButton.PressStarted += (s, e) => displayController.UpButtonState = true;
+            upButton.PressEnded += (s, e) => displayController.UpButtonState = false;
+        }
+        if (projectLab.DownButton is { } downButton)
+        {
+            downButton.PressStarted += (s, e) => displayController.DownButtonState = true;
+            downButton.PressEnded += (s, e) => displayController.DownButtonState = false;
+        }
+        if (projectLab.LeftButton is { } leftButton)
+        {
+            leftButton.PressStarted += (s, e) => displayController.LeftButtonState = true;
+            leftButton.PressEnded += (s, e) => displayController.LeftButtonState = false;
+        }
+        if (projectLab.RightButton is { } rightButton)
+        {
+            rightButton.PressStarted += (s, e) => displayController.RightButtonState = true;
+            rightButton.PressEnded += (s, e) => displayController.RightButtonState = false;
+        }
+
+        if (projectLab.Touchscreen is { } touchScreen)
+        {
+            touchScreen.TouchDown += (s, e) =>
+            {
+                Resolver.Log.Info("touch down");
+            };
+            touchScreen.TouchUp += (s, e) =>
+            {
+                Resolver.Log.Info("touch up");
+            };
+        }
+
+        Resolver.Log.Info("Initialization complete");
+
+        return base.Initialize();
+    }
+
+    private void OnTemperatureSensorUpdated(object sender, IChangeResult<Temperature> e)
+    {
+        Resolver.Log.Info($"TEMPERATURE: {e.New.Celsius:N1}C");
+        displayController.Temperature = e.New;
+    }
+
+    private void OnPressureSensorUpdated(object sender, IChangeResult<Pressure> e)
+    {
+        Resolver.Log.Info($"PRESSURE:    {e.New.Millibar:N1}mbar");
+        displayController.Pressure = e.New;
+    }
+
+    private void OnHumiditySensorUpdated(object sender, IChangeResult<RelativeHumidity> e)
+    {
+        Resolver.Log.Info($"HUMIDITY:    {e.New.Percent:N1}%");
+        displayController.RelativeHumidity = e.New;
+    }
+
+    private void OnLightSensorUpdated(object sender, IChangeResult<Illuminance> e)
+    {
+        Resolver.Log.Info($"LIGHT:       {e.New.Lux:N1}lux");
+        displayController.LightConditions = e.New;
+    }
+
+    private void OnAccelerometerUpdated(object sender, IChangeResult<Acceleration3D> e)
+    {
+        Resolver.Log.Info($"ACCEL:       {e.New.X.Gravity:0.0}, {e.New.Y.Gravity:0.0}, {e.New.Z.Gravity:0.0}g");
+        displayController.AccelerationConditions = e.New;
+    }
+
+    private void OnGyroscopeUpdated(object sender, IChangeResult<AngularVelocity3D> e)
+    {
+        Resolver.Log.Info($"GYRO:        {e.New.X.DegreesPerSecond:0.0}, {e.New.Y.DegreesPerSecond:0.0}, {e.New.Z.DegreesPerSecond:0.0}deg/s");
+        displayController.GyroConditions = e.New;
+    }
+
+    public override Task Run()
+    {
+        Resolver.Log.Info("Run...");
+
+        _ = audio?.PlaySystemSound(SystemSoundEffect.Success);
+
+        if (projectLab.TemperatureSensor is { } temperature)
+        {
+            temperature.StartUpdating(TimeSpan.FromSeconds(5));
+        }
+        if (projectLab.BarometricPressureSensor is { } barometer)
+        {
+            barometer.StartUpdating(TimeSpan.FromSeconds(5));
+        }
+        if (projectLab.HumiditySensor is { } humidity)
+        {
+            humidity.StartUpdating(TimeSpan.FromSeconds(5));
+        }
+        if (projectLab.LightSensor is { } luminance)
+        {
+            luminance.StartUpdating(TimeSpan.FromSeconds(5));
+        }
+
+        if (projectLab.Accelerometer is { } accelerometer)
+        {
+            accelerometer.StartUpdating(TimeSpan.FromSeconds(5));
+        }
+        if (projectLab.Gyroscope is { } gyroscope)
+        {
+            gyroscope.StartUpdating(TimeSpan.FromSeconds(5));
+        }
+
+        displayController?.Update();
+
+        Resolver.Log.Info("starting blink");
+        _ = projectLab.RgbLed?.StartBlink(WildernessLabsColors.PearGreen, TimeSpan.FromMilliseconds(500), TimeSpan.FromMilliseconds(2000), 0.5f);
+
+        return base.Run();
+    }
+}
